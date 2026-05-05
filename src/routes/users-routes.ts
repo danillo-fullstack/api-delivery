5e6d762334dcb49a9ad99bea3fd62a8fb94afc6e
Danillo <danilo_victor2004@hotmail.com>
2026-05-05 11:29:05 -0300

    feat: update package.json description, add user controller and routes

---
+++ src/routes/users-routes.ts
@@ -0,0 +1,9 @@
+import { Router } from "express";
+import { UserController } from "@/controllers/users-controller";
+
+const usersRoutes = Router();
+const usersController = new UserController();
+
+usersRoutes.post("/", usersController.create);
+
+export { usersRoutes };
