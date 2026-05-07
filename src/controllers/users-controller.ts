ad95bb910762b125b10721b4f06df2758af56d26
Danillo <danilo_victor2004@hotmail.com>
2026-05-06 23:40:26 -0300

    feat: add unique constraint on user email and creation user.

--- src/controllers/users-controller.ts
+++ src/controllers/users-controller.ts
@@ -2,19 +2,42 @@ import { Request, Response } from "express";
 import { hash } from "bcrypt";
 import { z } from "zod";
 
+import { prisma } from "@/database/prisma";
+import { AppError } from "@/utils/AppError";
+
 class UserController {
   async create(request: Request, response: Response) {
     const bodySchema = z.object({
       name: z.string().trim().min(2),
-      email: z.string().email(),
+      email: z.string().trim().email(),
       password: z.string().min(6),
     });
 
     const { name, email, password } = bodySchema.parse(request.body);
 
+    const userWithSameEmail = await prisma.user.findUnique({ where: { email } });
+
+    if (userWithSameEmail) {
+      throw new AppError("User with same email already exists.");
+    }
+
     const hashedPassword = await hash(password, 8);
 
-    return response.json({ message: "ok", hashedPassword });
+    const createdUser = await prisma.user.create({
+      data: {
+        name,
+        email,
+        password: hashedPassword,
+      },
+      select: {
+        id: true,
+        name: true,
+        email: true
+      }
+
+    });
+
+    return response.status(201).json(createdUser);
   }
 }
 
