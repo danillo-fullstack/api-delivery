46a93c17f567e8fe440d50609df0f85641b5ef0d
Danillo <danilo_victor2004@hotmail.com>
2026-05-07 18:27:16 -0300

    feat: implement user authentication in sessions controller

--- src/controllers/sessions-controller.ts
+++ src/controllers/sessions-controller.ts
@@ -1,8 +1,28 @@
 import { Request, Response } from "express";
+import { prisma } from "@/database/prisma";
+import { compare } from "bcrypt";
+import { z } from "zod";
+
+import { AppError } from "@/utils/AppError";
 
 class SessionsController {
-  create(request: Request, response: Response) {
-    return response.json({ message: "OK" });
+  async create(request: Request, response: Response) {
+    const bodySchema = z.object({
+      email: z.string().email().trim(),
+      password: z.string().min(6).trim(),
+    });
+
+    const { email, password } = bodySchema.parse(request.body);
+
+    const user = await prisma.user.findUnique({ where: { email } });
+
+    if (!user || !(await compare(password, user.password)))  {
+      throw new AppError("Invalid email or password", 401);
+    }
+
+    const { password: _, ...userWithoutPassword } = user;
+
+    return response.json({ user: userWithoutPassword });
   }
 }
 
